3dcadca343f016ef6dc3fb257a81c7c9a679bc3e
dbstocker <dstocker@notesettersinc.com>
2018-08-22 22:42:17 -0400

    commit before scrubbing cool styling to simplify element selections

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+// Metal Attack Game
+
+// global variables
+var playerHealth = 0;
+var enemyHealth = 0;
+
+// Avatar Objects
+var tomAraya = {
+  health: 125,
+  attack: 8
+}
+var scottIan = {
+  health: 140,
+  attack: 8
+}
+var daveMustaine = {
+  health: 155,
+  attack: 8
+}
+var alexSkolnick = {
+  health: 170,
+  attack: 8
+}
+
+// on click, choose your player
+
+// on click, choose your first opponent
+
+// on click, attack button calculates attack and counter attack results
+
+// *loop
+
+// if playerHealth <= 0
+
+// { lose == true && game over }
+
+// else if enemyHealth <= 0
+
+// { on click chose next opponent }
+
+// else
+
+// { on click play again }
+
+// console.log(alexSkolnick);
+
