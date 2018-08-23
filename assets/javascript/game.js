28f3bbf9d341129591c13c40d30adab961ea6ed9
dbstocker <dstocker@notesettersinc.com>
2018-08-23 08:56:33 -0400

    layout improvements to the wireframe

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,26 +7,40 @@ var enemyHealth = 0;
 // Avatar Objects
 var tomAraya = {
   health: 125,
-  attack: 8
+  attack: 8,
+  counterAttack: 13,
+  playerImg: "assets/images/tom-araya.jpg"
 }
 var scottIan = {
   health: 140,
-  attack: 8
+  attack: 12,
+  counterAttack: 10,
+  playerImg: "assets/images/scott-ian.jpg"
 }
 var daveMustaine = {
   health: 155,
-  attack: 8
+  attack: 10,
+  counterAttack: 10,
+  playerImg: "assets/images/dave-mustaine.jpg"
 }
 var alexSkolnick = {
   health: 170,
-  attack: 8
+  attack: 14,
+  counterAttack: 9,
+  playerImg: "assets/images/alex-skolnick.jpg"
 }
 
 // on click, choose your player
+$("#player-character").on("click", function () {
+  console.log("player selected")
+})
+  // move player out of top row and into bottom left position
+  // cause the shred button to appear below player image
 
 // on click, choose your first opponent
+  // move oponent out of top row and into bottom left positon, below player
 
-// on click, attack button calculates attack and counter attack results
+// on click, shred button calculates attack and counter attack results
 
 // *loop
 
