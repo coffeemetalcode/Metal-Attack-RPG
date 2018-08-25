186c783625cc8f6c16af48660fbe53ee9f32a29d
dbstocker <dstocker@notesettersinc.com>
2018-08-24 22:11:10 -0400

    getting some movement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,66 @@
 // global variables
 var playerHealth = 0;
 var enemyHealth = 0;
+var player;
+var defender;
 
-// Avatar Objects
-var tomAraya = {
-  health: 125,
-  attack: 8,
-  counterAttack: 13,
-  playerImg: "assets/images/tom-araya.jpg"
+// Contestants Object...
+var contestants = {
+  // ...with Avatar Objects inside
+  tomAraya: {
+    name: "Tom Araya",
+    health: 125,
+    attack: 8,
+    counterAttack: 13,
+    playerSlide: "<img class='player-img available-player' src='assets/images/tom-araya.jpg'>"
+    // nameHealth: { this.name + " " this.health }
+  },
+  scottIan: {
+    name: "Scott Ian",
+    health: 140,
+    attack: 12,
+    counterAttack: 10,
+    playerSlide: "<img class='player-img available-player' src='assets/images/scott-ian.jpg'>"
+  },
+  daveMustaine: {
+    name: "Dave Mustaine",
+    health: 155,
+    attack: 10,
+    counterAttack: 10,
+    playerSlide: "<img class='player-img available-player' src='assets/images/dave-mustaine.jpg'>"
+  },
+  alexSkolnick: {
+    name: "Alex Skolnick",
+    health: 170,
+    attack: 14,
+    counterAttack: 9,
+    playerSlide: "<img class='player-img available-player' src='assets/images/alex-skolnick.jpg'>"
+  }
 }
-var scottIan = {
-  health: 140,
-  attack: 12,
-  counterAttack: 10,
-  playerImg: "assets/images/scott-ian.jpg"
-}
-var daveMustaine = {
-  health: 155,
-  attack: 10,
-  counterAttack: 10,
-  playerImg: "assets/images/dave-mustaine.jpg"
-}
-var alexSkolnick = {
-  health: 170,
-  attack: 14,
-  counterAttack: 9,
-  playerImg: "assets/images/alex-skolnick.jpg"
-}
+
+// Push all players to .available-player area on doucment load
+// This can probably be achieved with a loop
+$(document).ready(function() {
+  $("#board-message").html("Choose your Avatar");
+  $("#player-1").html(contestants.tomAraya.playerSlide);
+  $("#player-1-info").html(contestants.tomAraya.name + " - " + contestants.tomAraya.health);
+  $("#player-2").html(contestants.scottIan.playerSlide);
+  $("#player-2-info").html(contestants.scottIan.name + " - " + contestants.scottIan.health);
+  $("#player-3").html(contestants.daveMustaine.playerSlide);
+  $("#player-3-info").html(contestants.daveMustaine.name + " - " + contestants.daveMustaine.health);
+  $("#player-4").html(contestants.alexSkolnick.playerSlide);
+  $("#player-4-info").html(contestants.alexSkolnick.name + " - " + contestants.alexSkolnick.health);
+})
 
 // on click, choose your player
-$("#player-character").on("click", function () {
-  console.log("player selected")
+$(".available-player").on("click", function () {
+  player = $(this);
+  $(this).hide();
+  $("#player-character").html(player.playerSlide);
+  $("#player-character-info").html(player);
+  console.log(player);
+  $("#board-message").html("Choose your Aversary");
+  console.log("player selected");
 })
   // move player out of top row and into bottom left position
   // cause the shred button to appear below player image
