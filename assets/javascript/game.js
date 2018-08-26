7afe628962492cecd02571424e781afd5d2fd4d7
dbstocker <dstocker@notesettersinc.com>
2018-08-25 23:39:40 -0400

    gonna have to be good enough for now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,11 @@ var playerHealth = 0;
 var defenderHealth = 0;
 var playerIsDead = false; // this is unnecessary
 var defenderIsDead = false; // this is also unnecessary
+var numDefenders = 3;
 
 // Avatar Objects
 var tomAraya = {
-  // id: tomAraya, // this looks like trouble
+  // id: tomAraya, // this seems like a bad idea
   name: "Tom Araya",
   health: 125,
   attack: 8,
@@ -68,27 +69,24 @@ $(document).ready(function () {
 // having trouble getting the below to work. I'm coming up empty with a way to cause the on click to push the clicked object into a new variable (player or defender). proceeding below with game logic.
 
 // // on click, choose your player
-// $(".available-player").on("click", function () {
-//   $("#board-message").html("Choose your Adversary");
-//   player = this;
-//   // $(player).hide();
-//   // $("#player-character").html();
-//   // $("#player-character-info").html();
-//   console.log(this);
-//   // console.log("player selected");
-// })
+$(".available-player").on("click", function () {
+  $("#board-message").html("Choose your Adversary");
+  // player = this;
+  // $(player).hide();
+  // $("#player-character").html();
+  // $("#player-character-info").html();
+  // console.log(this);
+  // console.log("player selected");
+})
 
-
-// move player out of top row and into bottom left position
-// cause the shred button to appear below player image
-
-// on click, choose your first opponent
-
-// move oponent out of top row and into bottom left positon, below player
+function result() {
+  console.log("player: " + player.name + " attack: " + attack + " health: " + playerHealth);
+  console.log("defender: " + defender.name + " attack: " + counterAttack + " health: " + defenderHealth);
+}
 
 function gamePlay() {
 
-  // the on click event should trigger pushing selected players into the variables player and defender.
+  // the on click event should trigger pushing selected players into the variables player and defender. For now, I'm playing around with it by changing the player and defender variables manually.
   player = scottIan;
   attack = player.attack;
   playerHealth = player.health;
@@ -97,25 +95,33 @@ function gamePlay() {
   counterAttack = defender.counterAttack;
   defenderHealth = defender.health;
 
-  $("#attack-btn").on("click", function () {
-    if (!(playerHealth < 1) || !(defenderHealth < 1)) {
-      playerHealth = (playerHealth - counterAttack);
-      defenderHealth = (defenderHealth - attack);
-      attack = (attack + player.attack);
-    }
-    if (playerHealth <= 0) {
-      console.log(player.name + " died");
-    }
-    else if (defenderHealth <= 0) {
-      console.log(defender.name + " died.");
-      defender = alexSkolnick;
-      console.log("new defender: " + defender.name);
-    }
-    console.log("player: " + player.name + " attack: " + attack + " health: " + playerHealth);
-    console.log("defender: " + defender.name + " attack: " + counterAttack + " health: " + defenderHealth);
-    defender = alexSkolnick;
-  })
-  console.log(defender.name);
+
+  for (i = 1; i < numDefenders; i++) {
+    $("#attack-btn").on("click", function () {
+      if (!(playerHealth < 1) || !(defenderHealth < 1)) {
+        playerHealth = (playerHealth - counterAttack);
+        defenderHealth = (defenderHealth - attack);
+        attack = (attack + player.attack);
+        result();
+      }
+      else if (playerHealth <= 0) {
+        result();
+        console.log(player.name + " died");
+      }
+      else if (defenderHealth <= 0) {
+        result();
+        console.log(defender.name + " died.");
+        defender = alexSkolnick;
+        console.log("new defender: " + defender.name);
+        console.log(defender.name)
+        console.log(defender.counterAttack);
+      }
+    })
+  }
 }
 
 gamePlay();
+
+// I really bit off a chunk here, going for the "challenge" assignment.
+// I'm going to keep hacking away at it until it works. Right now it's
+// somewhere between "oil burning jalopy" and "minimum viable product"
